884da6e09003a99a3ea8e465b9a4002408dc9b4a
Juan Methol (JMET.UK) <jmet@columbusglobal.com>
2017-06-15 11:17:48 +0100

    Agregué el método para crear la matriz.

--- Primer Obligatorio/Primer Obligatorio/Program.cs
+++ Primer Obligatorio/Primer Obligatorio/Program.cs
@@ -11,7 +11,7 @@ namespace Primer_Obligatorio
         {
             int opcion = 0;
             bool esnumero = false;
-
+            int cantidadclientes = 0;
             while (!esnumero)
             {
                 Console.Clear();
@@ -35,33 +35,63 @@ namespace Primer_Obligatorio
                 switch (opcion)
                 {
                     case 1:
-                        CantidadClientes();
+                        CantidadClientes(ref cantidadclientes);
+                        String[] apostadores = new String[cantidadclientes];
                         break;
                     case 2:
-                        Apuesta();
+                        //Apuesta();
                         break;
                     case 3:
-                        ApuestaSorpresa();
+                        // ApuestaSorpresa();
                         break;
                     case 4:
-                        EliminaApuesta();
+                        //EliminaApuesta();
                         break;
                     case 5:
-                        Listado();
+                        // Listado();
                         break;
                 }
             }
         }
 
-       
-        
-        public static void CantidadClientes()
+
+
+        public static void CantidadClientes(ref int cantidadclientes)
         {
+            //Pasa el entero de cantidad de clientes por referencia. Si fue cargado previamente, advierte al ususario que se ha eliminado. Tal vez agregar una pregunta para esto?
+
+            Console.Clear();
+
+            if (cantidadclientes != 0)
+            {
+                Console.WriteLine("Los valores de apuestas han sido eliminados");
+            }
+
+            Console.WriteLine("Ingrese la cantidad de apuestas");
+            bool esnumero = Int32.TryParse(Console.ReadLine(), out cantidadclientes);
+            Console.WriteLine("Se ingresaran {0} apuestas. \nPresione una tecla para continuar", cantidadclientes);
+            Console.ReadLine();
+
+            if (!esnumero)
+            {
+                Console.Write("La opción ingresada no es válida.");
+                Console.ReadLine();
+
+            }
+
+
         }
+
+        
 
         public static void Apuesta()
         {
+            Console.Clear();
+            Console.WriteLine("Ingrese el nombre del apostador");
+
         }
+
+        /*
 
         public static void ApuestaSorpresa()
         {
@@ -74,11 +104,6 @@ namespace Primer_Obligatorio
         public static void Listado()
         {
         }
-
-
-
-
-
-
+        */
     }
 }
