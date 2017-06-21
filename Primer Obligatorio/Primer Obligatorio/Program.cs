1e56c9ad25a1cacd8798b645f161bcb7989d2f75
juan-1603 <juanlopezmelsion@gmail.com>
2017-06-20 21:06:03 -0300

    Add files via upload

--- Primer Obligatorio/Primer Obligatorio/Program.cs
+++ Primer Obligatorio/Primer Obligatorio/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Primer_Obligatorio
+namespace ConsoleApplication1
 {
     class Program
     {
@@ -29,7 +29,7 @@ namespace Primer_Obligatorio
                 switch (opcion)
                 {
                     case 1:
-                        apostadores = CantidadClientes(ref apostadores, ref nombres, ref apuestas);
+                        apostadores = CantidadClientes(ref apostadores, ref nombres, ref apuestas, ref ultimoapostado);
                         break;
                     case 2:
                         Apuesta(ref ultimoapostado, ref nombres, ref apuestas);
@@ -55,22 +55,22 @@ namespace Primer_Obligatorio
 
         public static void MostrarMenu(int apostadores)
         {
-                Console.Clear();
-                Console.WriteLine("******************************************");
-                Console.WriteLine("               5 de Oro");
-                //Console.WriteLine("\n            733555555533352535555347             \n            7455555557575757575757553             \n            6877777777777777777777775             \n            8377777723253294449422243             \n            277777778866688888886689              \n            27777777                              \n            87 77777                              \n           58  77 77                              \n           68     77  7777777777                  \n           95                   777               \n           57                      7              \n                    7206695         7             \n            7  77728695346865        7            \n           4888883          777       7           \n                             77       7           \n                              7       7           \n                               7 7 7  7           \n         777777777            377777777           \n         577777777           7777777777           \n         75777777777         777777777            \n          757777777777777777777777775             \n           5577777777777577777777725              \n            74427777777777777775407               \n              3888935777552468867                 \n                 7368888888827      ");
-                Console.WriteLine("****************************************** \n");
-                Console.WriteLine("   1 - Ingresar la cantidad de clientes    " + apostadores);
-                Console.WriteLine("   2 - Ingresar apuesta");
-                Console.WriteLine("   3 - Ingresar apuesta sorpresa");
-                Console.WriteLine("   4 - Eliminar apuesta");
-                Console.WriteLine("   5 - Listados");
-                Console.WriteLine("   6 - Salir\n");
-                Console.WriteLine("******************************************");
-                Console.Write("Ingrese la opción deseada: ");
+            Console.Clear();
+            Console.WriteLine("******************************************");
+            Console.WriteLine("               5 de Oro");
+            //Console.WriteLine("\n            733555555533352535555347             \n            7455555557575757575757553             \n            6877777777777777777777775             \n            8377777723253294449422243             \n            277777778866688888886689              \n            27777777                              \n            87 77777                              \n           58  77 77                              \n           68     77  7777777777                  \n           95                   777               \n           57                      7              \n                    7206695         7             \n            7  77728695346865        7            \n           4888883          777       7           \n                             77       7           \n                              7       7           \n                               7 7 7  7           \n         777777777            377777777           \n         577777777           7777777777           \n         75777777777         777777777            \n          757777777777777777777777775             \n           5577777777777577777777725              \n            74427777777777777775407               \n              3888935777552468867                 \n                 7368888888827      ");
+            Console.WriteLine("\n****************************************** \n");
+            Console.WriteLine("   1 - Ingresar la cantidad de clientes    " + apostadores);
+            Console.WriteLine("   2 - Ingresar apuesta");
+            Console.WriteLine("   3 - Ingresar apuesta sorpresa");
+            Console.WriteLine("   4 - Eliminar apuesta");
+            Console.WriteLine("   5 - Listados");
+            Console.WriteLine("   6 - Salir\n");
+            Console.WriteLine("******************************************");
+            Console.Write("Ingrese la opción deseada: ");
         }
 
-        public static int CantidadClientes(ref int apostadores, ref string[] nombres, ref int[,] apuestas)
+        public static int CantidadClientes(ref int apostadores, ref string[] nombres, ref int[,] apuestas, ref int ultimoapostado)
         {
             Console.Clear();
 
@@ -84,6 +84,7 @@ namespace Primer_Obligatorio
                     apostadores = Convert.ToInt32(Console.ReadLine());
                     nombres = new string[apostadores];
                     apuestas = new int[apostadores, 5];
+                    ultimoapostado = 0;
                     return apostadores;
                 }
                 else
@@ -106,38 +107,46 @@ namespace Primer_Obligatorio
         public static void Apuesta(ref int ultimoapostado, ref string[] nombres, ref int[,] apuestas)
         {
             Console.Clear();
-            Console.WriteLine("Ingrese el nombre del apostador");
-            
+            Console.Write("Ingrese el nombre del apostador: ");
+
             //falta validar si el nombre no esta escrito en otro campo y que sea tengo tope por cantidad de apuestas
 
             nombres[ultimoapostado] = Console.ReadLine();
-            for(int i=0; i<5;i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("Ingrese el {0}º valor de la apuesta", i+1);
-                bool esnumero = Int32.TryParse(Console.ReadLine(), out apuestas[ultimoapostado,i]);
-                if (!esnumero)
+                bool esnumero = false;
+                while (!esnumero)
                 {
-                    Console.Write("La opción ingresada no es válida.\nIngrese nuevamente el {0}º valor de la apuesta", i+1);
-                    Console.ReadLine();
+                    Console.Write("\nIngrese el {0}º valor de la apuesta: ", i + 1);
+                    esnumero = Int32.TryParse(Console.ReadLine(), out apuestas[ultimoapostado, i]);
+                    if (!esnumero)
+                    {
+                        Console.WriteLine("\nLa opción ingresada no es válida.");
+                    }
+                    else if (apuestas[ultimoapostado, i] < 1 || apuestas[ultimoapostado, i] > 49)
+                    {
+                        Console.WriteLine("\nLa opción ingresada no es válida.");
+                        i--;
+                    }
                 }
             }
             ultimoapostado++;
         }
 
-        
+
         public static void ApuestaSorpresa(ref int ultimoapostado, ref string[] nombres, ref int[,] apuestas)
         {
             Console.Clear();
             Console.WriteLine("Ingrese el nombre del apostador");
-            
+
             //falta validar si el nombre no esta escrito en otro campo y que sea tengo tope por cantidad de apuestas
 
             nombres[ultimoapostado] = Console.ReadLine();
 
-            for(int i=0; i<5;i++)
+            for (int i = 0; i < 5; i++)
             {
                 Random variable = new Random();
-                apuestas[ultimoapostado,i]=variable.Next(1, 49);
+                apuestas[ultimoapostado, i] = variable.Next(1, 49);
                 System.Threading.Thread.Sleep(10);
             }
 
@@ -205,19 +214,19 @@ namespace Primer_Obligatorio
             //falta validar si el nombre no está
 
             bool buscando = true;
-            int i=0;
+            int i = 0;
             while (buscando)
             {
-                if(nombres[i] == nombrebuscado)
+                if (nombres[i] == nombrebuscado)
                 {
                     buscando = false;
                     Console.WriteLine("Las apuestas de {0} son:", nombres[i]);
-                    for (int j=0; j < apuestas.GetLength(1); j++)
+                    for (int j = 0; j < apuestas.GetLength(1); j++)
                     {
                         Console.Write("{0},", apuestas[i, j]);
                     }
                 }
-                else 
+                else
                 {
                     if (i == nombres.Length)
                     {
@@ -229,11 +238,11 @@ namespace Primer_Obligatorio
                         i++;
                     }
                 }
-                
+
             }
             Console.ReadKey();
 
-            
+
 
         }
 
@@ -241,24 +250,36 @@ namespace Primer_Obligatorio
         public static void ListadoCompletoApuestas(ref string[] nombres, ref int[,] apuestas)
         {
             Console.Clear();
-            for (int k = 0; k<nombres.Length; k++)
+            for (int k = 0; k < nombres.Length; k++)
             {
                 Console.WriteLine(nombres[k], ":\t");
-                for (int j = 0; j<apuestas.GetLength(1); j++)
+                for (int j = 0; j < apuestas.GetLength(1); j++)
                 {
                     Console.Write("\t " + apuestas[k, j]);
                 }
                 Console.WriteLine();
             }
             Console.ReadLine();
-
-
         }
 
-
-
-
-
-
+        public static int PosicionLibre(string[] nombres)
+        {
+            int posicionlibre = 0;
+            bool encontrado = false;
+            int i = 0;
+            while (!encontrado && i < nombres.Length)
+            {   
+                if (nombres[i] == "")
+                {
+                    encontrado = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            posicionlibre = i;
+            return posicionlibre;
+        }
     }
 }
